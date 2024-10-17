d8185c61e5de514ebbc28fa9dc29a519da219c08
Rauli Kaksonen <rauli.kaksonen@testofthings.com>
2024-10-17 15:26:50 +0300

    More complete reading/writing code. Does not look that great any more

--- tdsaf/serializer/serializer.py
+++ tdsaf/serializer/serializer.py
@@ -1,7 +1,7 @@
 """Serializer"""
 
 import json
-from typing import Any, Callable, Dict, Iterable, List, Optional, Self, Type, TypeVar
+from typing import Any, Callable, Dict, Iterable, List, Optional, Self, Set, Type, TypeVar
 from tdsaf.core.model import Host, IoTSystem, NetworkNode, Service
 
 T = TypeVar('T')
@@ -9,15 +9,24 @@ T = TypeVar('T')
 
 class SerializerContext:
     """Serializer context"""
-    def __init__(self, body: Any, class_map: Dict[str, Type] = None):
+    def __init__(self, body: Any):
         self.body = body
         self.parent: Optional[SerializerContext] = None
         self.identifiers: Dict[str, SerializerContext] = {}
         self.reverse_ids: Dict[Any, str] = {}
-        self.class_map = {} if class_map is None else class_map
+        self.class_map = {}
+        self.constructors: Dict[Type, Callable[[SerializerContext], SerializerContext]] = {}
         # per body
         self.simple_attributes: Dict[str, str] = {}
-        self.list_attributes: Dict[str, List[Any]] = {}
+        self.sinks: Dict[Type, List[Any]] = {}
+
+    def map(self, name: str, class_: Type,
+            constructor: Optional[Callable[['SerializerContext'], Any]] = None,) -> 'SerializerContext':
+        """Map class to string"""
+        self.class_map[name] = class_
+        if constructor:
+            self.constructors[class_] = constructor
+        return self
 
     def add(self, body: Any) -> 'SerializerContext':
         """Add object to serialize"""
@@ -31,10 +40,11 @@ class SerializerContext:
         if i:
             return self.identifiers[i]  # not added
         i = str(len(self.identifiers))
-        ctx = SerializerContext(body, self.class_map)
+        ctx = SerializerContext(body)
         ctx.parent = self
         ctx.identifiers = self.identifiers
         ctx.reverse_ids = self.reverse_ids
+        ctx.class_map = self.class_map
         self.identifiers[i] = ctx
         self.reverse_ids[body] = i
         return ctx
@@ -45,10 +55,18 @@ class SerializerContext:
             self.simple_attributes[a] = a
         return self
 
-    def list(self, attribute: List[T], field:str) -> List[T]:
+    def list(self, attribute: List[T], types: Set[Type]) -> List[T]:
         """Handle list of objects"""
-        self.list_attributes[field] = attribute
+        for t in types:
+            self.sinks[t] = attribute
         return attribute
+
+    def __getitem__(self, key: str) -> Any:
+        return getattr(self.body, key)
+
+    def get_parent(self) -> Optional[Any]:
+        """Get parent object, if any"""
+        return self.parent.body if self.parent else None
 
     def write_json(self) -> Iterable[Dict]:
         """Write to JSON array"""
@@ -56,6 +74,36 @@ class SerializerContext:
 
         for i, ctx in self.identifiers.items():
             yield ctx.as_json_(i, class_reverse)
+
+    def read_json_next(self, data: Dict) -> Any:
+        """Read next object from JSON stream"""
+        read_id = data["id"]
+        parent_id = data.get("at")
+        class_str = data["type"]
+        class_ = self.class_map.get(class_str)
+        assert class_, f"Class {class_str} not in class map"
+        if parent_id is not None:
+            parent = self.identifiers.get(parent_id)
+            assert parent, f"Parent {parent_id} not found"
+        else:
+            parent = self
+        constructor = self.constructors.get(class_)
+        if constructor:
+            ctx = constructor(parent)
+            body = ctx.body
+        else:
+            body = class_()  # default constructor
+            ctx = self.add(body)
+        for k, f in self.simple_attributes.items():
+            if k in data:
+                setattr(body, f, data.get(k))
+        # new context
+        self.identifiers[read_id] = ctx
+        self.reverse_ids[body] = read_id
+        sink = parent.sinks.get(class_)
+        if sink is not None:
+            sink.append(body)
+        return body
 
     def as_json_(self, identifier: str, class_reverse: Dict[Type, str]) -> Dict:
         """Convert to JSON. Internal method"""
@@ -66,14 +114,14 @@ class SerializerContext:
             "type": class_str,
         }
         if self.parent:
-            body_dict["parent"] = self.reverse_ids[self.parent.body]
+            body_dict["at"] = self.reverse_ids[self.parent.body]
         for k, f in self.simple_attributes.items():
-            body_dict[k] = getattr(self.body, f)
+            body_dict[k] = self[f]
         return body_dict
 
     def __repr__(self) -> str:
         class_reverse = {v: k for k, v in self.class_map.items()}
-        d = self.as_json_(self.reverse_ids.get(self.body, "?"), class_reverse)        
+        d = self.as_json_(self.reverse_ids.get(self.body, "?"), class_reverse)
         return json.dumps(d)
 
 
@@ -82,17 +130,17 @@ class SystemSerializer():
 
     # pylint: disable=missing-function-docstring
 
-    def network_node(self, body: NetworkNode, context: SerializerContext):
+    def node(self, body: NetworkNode, context: SerializerContext) -> SerializerContext:
         ctx = context.add(body)
         ctx.default("name")
-        for c in ctx.list(body.children, "nodes"):
-            self.network_node(c, ctx)
+        for c in ctx.list(body.children, {Host, Service}):
+            self.node(c, ctx)
+        return ctx
 
     def iot_system(self, body: IoTSystem) -> SerializerContext:
-        context = SerializerContext(body, class_map={
-            "system": IoTSystem,
-            "host": Host,
-            "service": Service,
-        })
-        self.network_node(body, context)
-        return context
+        ctx = SerializerContext(body)
+        ctx.map("host", Host, lambda c: self.node(Host(body, c["name"]), c))
+        ctx.map("service", Service, lambda c: self.node(Service(c["name"], c.get_parent()), c))
+        ctx.map("system", IoTSystem)
+        self.node(body, ctx)
+        return ctx
--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -1,10 +1,19 @@
+from tdsaf.core.model import IoTSystem
 from tdsaf.serializer.serializer import SystemSerializer
 from tests.test_model_new import Setup_1
 
 
 def test_simple_model():
     su = Setup_1()
-    seralizer = SystemSerializer()
-    ctx = seralizer.iot_system(su.get_system())
-    js = list(ctx.write_json())
+    su.system.system.name = "Test"
+    serializer = SystemSerializer()
+    writer = serializer.iot_system(su.get_system())
+    js = list(writer.write_json())
     assert js
+
+    reader = serializer.iot_system(IoTSystem())
+    r = []
+    for j in js:
+        obj = reader.read_json_next(j)
+        r.append(obj)
+    assert r
