86947c19f492fbb4d81a60bdec8a7dfa5bece85d
Zabrakk <ak4.jarvenpaa@gmail.com>
2025-01-07 16:35:42 +0200

    Updated ShodanScan: - Divided functionalities into their own methods - Adds hearbleed verdict to service - Adds HTTP(s) status code to service - Adds CPE info to service's parent's SW component

--- tdsaf/adapters/shodan_scan.py
+++ tdsaf/adapters/shodan_scan.py
@@ -5,18 +5,22 @@ import json
 import argparse
 from io import BufferedReader
 from pathlib import Path
-from typing import List, Dict, Union, Any, Set, cast
+from typing import List, Dict, Union, Tuple, Any, Set, cast
 from shodan.client import Shodan
 from shodan.exception import APIError
 
+from tdsaf.main import ConfigurationException
 from tdsaf.adapters.tools import SystemWideTool
-from tdsaf.core.model import IoTSystem, Service
+from tdsaf.core.model import IoTSystem, Service, NetworkNode
+from tdsaf.core.components import Software, SoftwareComponent
 from tdsaf.core.event_interface import EventInterface, PropertyEvent
 from tdsaf.common.traffic import EvidenceSource, Evidence, ServiceScan
-from tdsaf.main import ConfigurationException
 from tdsaf.common.address import IPAddress, Protocol, EndpointAddress
 from tdsaf.common.property import Properties, PropertyKey
 from tdsaf.common.verdict import Verdict
+
+# FIXME: DOCUMENT SHODAN
+# FIXME: UTEST
 
 
 class ShodanScan(SystemWideTool):
@@ -25,48 +29,96 @@ class ShodanScan(SystemWideTool):
         super().__init__("shodan", system)
         self.tool.name = "Shodan tool"
         self.data_file_suffix = ".json"
+        self.key_set: Set[PropertyKey]
+        self._interface: EventInterface
+        self._evidence: Evidence
+        self._key = Properties.FUZZ
 
     def determine_protocol(self, entry: Dict[str, Any]) -> Protocol:
         """Determine used protocol"""
-        if "ssl" in entry or "tls" in entry:
+        module: List[str] = entry["_shodan"]["module"].lower().split("-")
+        if "https" in module:
             return Protocol.TLS
-        if "http" in entry:
-            return Protocol.HTTP
-        if (protocol := Protocol.get_protocol(entry["product"])) is None:
-            raise ConfigurationException(f"Protocol {entry['product']} not defined")
-        return protocol
+        for protocol in Protocol:
+            if protocol.value in module:
+                return protocol
+        raise ConfigurationException(f"Could not determine protocol from Shodan module {module}")
+
+    def get_open_port_info(self, entry: Dict[str, Any]) -> Tuple[int, Protocol, Protocol]:
+        """Add open ports to endpoint"""
+        port: int = entry["port"]
+        transport = cast(Protocol, Protocol.get_protocol(entry["transport"]))
+        protocol = self.determine_protocol(entry)
+        return port, transport, protocol
+
+    def add_http_status(self, protocol: Protocol, entry: Dict[str, Any], service: Service) -> None:
+        """Add HTTP(S) status code to a HTTP(S) service's properties"""
+        if protocol in [Protocol.HTTP, Protocol.TLS]:
+            status_code = str(entry["http"]["status"])
+            key_part = "http" if protocol == Protocol.HTTP else "https"
+            key = PropertyKey("shodan", key_part, "status", status_code)
+            prop_addr_event = PropertyEvent(self._evidence, service, key.verdict(Verdict.PASS))
+            self._interface.property_update(prop_addr_event)
+
+    def add_vulnerabilities(self, vulnerabilities: Dict[str, Any], service: Service) -> None:
+        """Add vulnerability to service's properties"""
+        for vulnerability in vulnerabilities:
+            if (key := PropertyKey(self.tool_label, vulnerability)) not in self.key_set:
+                self.key_set.add(key)
+                veridct = Verdict.FAIL
+                prop_event = PropertyEvent(self._evidence, service, key.verdict(veridct))
+                self._interface.property_update(prop_event)
+
+    def add_heartbleed(self, opts: Dict[str, str], service: Service) -> None:
+        """Add Heartbleed verdict to service's properties"""
+        if "heartbleed" in opts and (key := PropertyKey(self.tool_label, "heartbleed")) not in self.key_set:
+            self.key_set.add(key)
+            comment = opts["heartbleed"].strip().split(" - ")[1]
+            verdict = Verdict.PASS if comment.upper() == "SAFE" else Verdict.FAIL
+            prop_event = PropertyEvent(self._evidence, service, key.verdict(verdict, comment))
+            self._interface.property_update(prop_event)
+
+    def add_cpes(self, cpe23: List[str], service: Service) -> None:
+        """Add Common Platform Enumeration info to SW component and assign verdict based on statement SBOM"""
+        parent = cast(NetworkNode, service.parent)
+        if len(parent.components) > 0:
+            parent_sw = cast(Software, parent.components[0])
+            for entry in cpe23:
+                software_str, version = entry.split(":")[-2:]
+                software = SoftwareComponent(software_str)
+                verdict = Verdict.PASS if software in parent_sw.components.values() else Verdict.FAIL
+                key = PropertyKey("component", software_str)
+                self._interface.property_update(
+                    PropertyEvent(self._evidence, parent_sw, key.verdict(verdict, f"v{version}, Shodan CPE 2.3"))
+                )
 
     def process_file(self, data: BufferedReader, file_name: str,
                      interface: EventInterface, source: EvidenceSource) -> bool:
         """Process file"""
-        evidence = Evidence(source)
+        self._interface = interface
+        self._evidence = Evidence(source)
 
         scan = cast(Dict[str, Any], json.load(data))
         ip_addr = IPAddress.new(file_name.split("-")[-1].replace(self.data_file_suffix, ""))
 
         entry: Dict[str, Any]
         for entry in scan.get("data", []):
-            # Open ports
-            port: int = entry["port"]
-            protocol = self.determine_protocol(entry)
-            transport = cast(Protocol, Protocol.get_protocol(entry["transport"]))
+            self.key_set = set()
 
+            port, transport, protocol = self.get_open_port_info(entry)
             endpoint_addr = EndpointAddress(ip_addr, transport, port)
-            service_scan = ServiceScan(evidence, endpoint_addr, protocol.value)
-            a: Service = interface.service_scan(service_scan)
+            service: Service = interface.service_scan(
+                ServiceScan(self._evidence, endpoint_addr, protocol.value)
+            )
 
-            # Vulnerabilities
-            key_set: Set[PropertyKey] = set()
-            for vulnerability in entry.get("vulns", {}):
-                if (key := PropertyKey(self.tool_label, vulnerability)) not in key_set:
-                    key_set.add(key)
-                    veridct = Verdict.FAIL
-                    ev = PropertyEvent(evidence, a, key.verdict(veridct))
-                    interface.property_update(ev)
+            self.add_http_status(protocol, entry, service)
+            self.add_vulnerabilities(entry.get("vulns", {}), service)
+            self.add_heartbleed(entry.get("opts", {}), service)
+            self.add_cpes(entry.get("cpe23", []), service)
 
-            if self.send_events:
-                ev = PropertyEvent(evidence, a, Properties.VULNERABILITIES.value_set(key_set))
-                interface.property_update(ev)
+            if self.send_events and self.key_set:
+                prop_event = PropertyEvent(self._evidence, service, Properties.VULNERABILITIES.value_set(self.key_set))
+                interface.property_update(prop_event)
 
         return True
 
