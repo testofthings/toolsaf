247b6d2fe6c4b93f2e6dbf8eda4103a96e68d296
Zabrakk <ak4.jarvenpaa@gmail.com>
2025-01-16 10:10:34 +0200

    Added a test for PCAPReader

--- tests/adapters/test_pcap_reader.py
+++ tests/adapters/test_pcap_reader.py
@@ -1,11 +1,12 @@
 import pathlib
 from tdsaf.core.inspector import Inspector
 
-from tdsaf.core.matcher import SystemMatcher
 from tdsaf.core.model import IoTSystem
 from tdsaf.adapters.pcap_reader import PCAPReader
 from tdsaf.core.registry import Registry
-from tdsaf.common.traffic import IPFlow
+from tdsaf.common.traffic import IPFlow, EvidenceSource
+from tdsaf.common.address import IPAddress
+from tests.test_model import Setup
 
 
 def test_pcap():
@@ -22,3 +23,47 @@ def test_pcap():
     c = m.connection(IPFlow.udp_flow(source_hw="86:df:86:76:53:bd", target_hw="dc:a6:32:28:34:e3", target_port=53))
     c_flows = cs[c]
     assert len(c_flows) == 2
+
+
+def _connections(entity):
+    connections = []
+    for connection in entity.connections:
+        connections.append((connection.source, connection.target.parent, connection.target.name))
+    return connections
+
+
+def test_process_file():
+    setup = Setup()
+    reader = PCAPReader(setup.get_system())
+
+    device = setup.system.device("Device")
+    app = setup.system.mobile("App")
+    backend_1 = setup.system.backend("Backend1")
+    backend_2 = setup.system.backend("Backend2")
+
+    device.new_address_(IPAddress.new("10.42.0.184"))
+    app.new_address_(IPAddress.new("10.42.0.133"))
+    backend_1.new_address_(IPAddress.new("18.193.211.120"))
+    backend_2.new_address_(IPAddress.new("18.194.10.142"))
+
+    #app >> device / TCP(port=6668)
+    #app >> backend_1 / TLS
+    #app >> backend_2 / TLS(port=8883)
+
+    source = EvidenceSource("pcap", "test")
+    with pathlib.Path("tests/samples/pcap/test.pcap").open("rb") as f:
+        reader.process_file(f, "", setup.get_inspector(), source)
+
+        conn = _connections(device.entity)
+        assert (app.entity, device.entity, "TCP:6668") in conn
+
+        conn = _connections(app.entity)
+        assert (app.entity, device.entity, "TCP:6668") in conn
+        assert (app.entity, backend_1.entity, "TCP:443") in conn
+        assert (app.entity, backend_2.entity, "TCP:8883") in conn
+
+        conn = _connections(backend_1.entity)
+        assert (app.entity, backend_1.entity, "TCP:443") in conn
+
+        conn = _connections(backend_2.entity)
+        assert (app.entity, backend_2.entity, "TCP:8883") in conn
