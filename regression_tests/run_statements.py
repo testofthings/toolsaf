6585de59e97f9ce709585747567de07bb81e1a57
Zabrakk <ak4.jarvenpaa@gmail.com>
2025-01-30 14:41:21 +0200

    Added GitHub action for the regression test + action summary creation

--- regression_tests/run_statements.py
+++ regression_tests/run_statements.py
@@ -8,6 +8,7 @@ from typing import Dict, Union, List
 
 FILE_PATH = Path(__file__)
 STATEMENTS_PATH = FILE_PATH.parent / "statements"
+GITHUB_SUMMARY = os.environ.get('GITHUB_STEP_SUMMARY', None)
 
 
 def read_setup(setup_file_path: Path) -> List[Dict[str, Union[str, List[str]]]]:
@@ -55,10 +56,36 @@ def run_statement(url: str, product: str) -> None:
             cwd=STATEMENTS_PATH
         )
         print(f"{dir_name} run passed")
+        if GITHUB_SUMMARY:
+            summarize_passed(dir_name)
     except subprocess.CalledProcessError as e:
         print(f"{dir_name} run failed! Error was:")
         print(e.stderr)
+        if GITHUB_SUMMARY:
+            summarize_failed(dir_name, str(e.stderr))
         sys.exit(1)
+
+
+def summarize_passed(statement: str) -> None:
+    add_to_summary(
+        f"### {statement}",
+        "Status: **Passed**"
+    )
+
+
+def summarize_failed(statement: str, error: str) -> None:
+    add_to_summary(
+        f"### {statement}",
+        "Status: **Failed**",
+        "Error was:",
+        f"```{error}```"
+    )
+
+
+def add_to_summary(*text: str) -> None:
+    with open(GITHUB_SUMMARY, 'a') as f:
+        for line in text:
+            print(line, file=f)
 
 
 def process_entry(entry: Dict[str, Union[str, List[str]]]) -> None:
@@ -71,6 +98,8 @@ def main(setup_file_path_str: str) -> None:
     """Perform test"""
     setup = read_setup(Path(setup_file_path_str))
     create_statements_dir()
+    if GITHUB_SUMMARY:
+        add_to_summary("# Test Summary:")
     for entry in setup:
         process_entry(entry)
 
