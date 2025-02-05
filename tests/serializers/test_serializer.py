7bfc936cbcd02a933cab65553e65cc3334ed3264
Rauli Kaksonen <rauli.kaksonen@testofthings.com>
2025-02-05 11:08:27 +0200

    Serializer tests to include reading

--- tests/serializers/test_serializer.py
+++ tests/serializers/test_serializer.py
@@ -7,10 +7,15 @@ def test_a_class():
     # 'obj' of class AClass is the object to write into 'json'
     ser = AClassSerializer()
     stream = SerializerStream(ser)
-    r = list(stream.write(obj))
-    assert r == [
+    js = list(stream.write(obj))
+    assert js == [
         {"id": "id1", "a_string": "AAA", "a_int": 2000}
     ]
+    ser = AClassSerializer()
+    stream = SerializerStream(ser)
+    read = list(stream.read(js))
+    assert len(read) == 1
+
 
 def test_b_class():
     obj = BClass()
@@ -21,18 +26,30 @@ def test_b_class():
     ]
     ser = BClassSerializer()
     stream = SerializerStream(ser)
-    r = list(stream.write(obj))
-    assert r == [
+    js = list(stream.write(obj))
+    assert js == [
         {"id": "id1"},
         {"id": "id2", "type": "a-type", "at": "id1", "a_string": "First", "a_int": 101},
         {"id": "id3", "type": "a-type", "at": "id1", "a_string": "Second", "a_int": 102},
         {"id": "id4", "type": "a-type", "at": "id1", "a_string": "Third", "a_int": 103},
     ]
+    ser = BClassSerializer()
+    stream = SerializerStream(ser)
+    read = list(stream.read(js))
+    read_b = read[0]
+    assert isinstance(read_b, BClass)
+    assert read_b.sub_instances == read[1:]
+    assert read[1].a_string == "First"
+    assert read[1].a_int == 101
+    assert read[2].a_string == "Second"
+    assert read[2].a_int == 102
+    assert read[3].a_string == "Third"
+    assert read[3].a_int == 103
 
 
 class AClass:
     """Test class A for serialization"""
-    def __init__(self, a_string: str, a_int: int) -> None:
+    def __init__(self, a_string: str = "", a_int: int = 0) -> None:
         self.a_string = a_string
         self.a_int = a_int
 
@@ -43,8 +60,9 @@ class AClassSerializer(Serializer[AClass]):
         self.config.map_simple_fields("a_string", "a_int")
 
     def read(self, obj: AClass, stream: SerializerStream) -> None:
-        parent = stream.resolve(of_type=BClass)
-        parent.sub_instances.append(obj)
+        parent = stream.resolve_optional(of_type=BClass)
+        if parent:
+            parent.sub_instances.append(obj)
 
 class BClass:
     """Test class B for serializer"""
