86efbf9765d2d214600e35194beb1851016176f7
Rauli <rauli.kaksonen@testofthigs.com>
2026-01-08 14:11:23 +0200

    Clearer LoggedEvent verdict handling. Fail anywhere overrides. Unit tests

--- tests/test_event.py
+++ tests/test_event.py
@@ -2,7 +2,8 @@ from toolsaf.common.address import Addresses, DNSName, EndpointAddress, Protocol
 from toolsaf.common.verdict import Verdict
 from toolsaf.builder_backend import SystemBackend
 from toolsaf.core.event_interface import PropertyAddressEvent, PropertyEvent
-from toolsaf.main import DNS
+from toolsaf.core.event_logger import LoggingEvent
+from toolsaf.main import DNS, TLS
 from toolsaf.common.property import Properties, PropertyKey
 from toolsaf.core.services import NameEvent
 from toolsaf.common.traffic import Evidence, EvidenceSource, IPFlow
@@ -108,3 +109,39 @@ def test_ipflow_event():
     evi = Evidence(EvidenceSource("Source A"))
     p2 = IPFlow.decode_data_json(evi, js, lambda x: None)
     assert p == p2
+
+
+def test_ipflow_log_event_verdict():
+    sb = SystemBackend()
+    conn = sb.device() >> sb.device() / TLS
+
+    p = IPFlow.UDP("1:0:0:0:0:1", "192.168.0.1", 1100) >> ("1:0:0:0:0:2", "192.168.0.2", 1234)
+
+    log_ev = LoggingEvent(p)
+    log_ev.pick_entity_verdict(conn.connection)
+    assert log_ev.resolve_verdict() == Verdict.INCON
+
+    log_ev.property_value = PropertyKey("non-verdict"), Verdict.FAIL
+    assert log_ev.resolve_verdict() == Verdict.INCON
+    log_ev.property_value = Properties.MITM.verdict(Verdict.INCON)
+    assert log_ev.resolve_verdict() == Verdict.INCON
+    log_ev.property_value = Properties.MITM.verdict(Verdict.PASS)
+    assert log_ev.resolve_verdict() == Verdict.PASS
+    log_ev.property_value = Properties.MITM.verdict(Verdict.FAIL)
+    assert log_ev.resolve_verdict() == Verdict.FAIL
+
+    conn.connection.set_seen_now()  # to set default EXPECTED property
+    log_ev = LoggingEvent(p)
+    log_ev.pick_entity_verdict(conn.connection)
+    assert log_ev.resolve_verdict() == Verdict.PASS
+
+    log_ev.property_value = Properties.MITM.verdict(Verdict.FAIL)
+    assert log_ev.resolve_verdict() == Verdict.FAIL
+
+    Properties.MITM.put_verdict(p.properties, Verdict.FAIL)
+    log_ev = LoggingEvent(p)
+    log_ev.pick_entity_verdict(conn.connection)
+    assert log_ev.resolve_verdict() == Verdict.FAIL
+
+    log_ev.property_value = Properties.MITM.verdict(Verdict.PASS)
+    assert log_ev.resolve_verdict() == Verdict.FAIL
